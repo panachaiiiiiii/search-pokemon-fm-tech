dfb308761c5086dfe8d3826941000eb1d242b0ac
Panachai <gasza60@gmail.com>
2026-05-16 04:03:30 +0700

    Update :  redirect to home

--- app/graphql/getPokemon.ts
+++ app/graphql/getPokemon.ts
@@ -12,6 +12,50 @@ export const GET_POKEMON = gql`
         minimum
         maximum
       }
+      height {
+        minimum
+        maximum
+      }
+      classification
+      types
+      resistant
+      weaknesses
+      fleeRate
+      maxCP
+      maxHP
+      attacks {
+        fast {
+          name
+          type
+          damage
+        }
+        special {
+          name
+          type
+          damage
+        }
+      }
+      evolutions {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+export const GET_POKEMON_20 = gql`
+  query GetPokemon($first: Int!) {
+    pokemon(first: $first) {
+      id
+      number
+      name
+      image
+
+      weight {
+        minimum
+        maximum
+      }
 
       height {
         minimum
