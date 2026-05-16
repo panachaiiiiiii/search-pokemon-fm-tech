b742c0e7439163ef38b3421ae94b4da7efa19fbd
Panachai <gasza60@gmail.com>
2026-05-16 20:07:10 +0700

    config:cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.pokemondb.net",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
